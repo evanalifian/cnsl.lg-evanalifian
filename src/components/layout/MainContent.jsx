81e7b2bf20ebcb522dc145811d0786a146c9697f
evanalifian <evanalifian@gmal.com>
2025-07-16 16:48:40 +0700

    remove padding bottom

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,6 +1,6 @@
 export default function MainContent({ children }) {
   return (
-    <main className="mx-auto w-full max-w-6xl px-3 pb-[1000px]">
+    <main className="mx-auto w-full max-w-6xl px-4">
       <div className="border-x border-dashed border-zinc-300 px-4">
         {children}
       </div>
